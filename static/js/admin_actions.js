bd96dcf90c4e368e7d304341116be15d906aa908
Wien Margarete <reymundo.estevesii@gmail.com>
2026-03-13 11:44:41 +0800

    Remove unused admin_actions.js

--- static/js/admin_actions.js
+++
@@ -1,1 +0,0 @@
-/* admin_actions.js - Functions merged into script.js. This file is no longer needed. */\n\n// All functions (confirmAdminAction, showAdminActionModal, etc.) now in static/js/script.js
