3d1d27b328f15919bdb6ea7abefd3e21be704dfa
Reymundo <reymundo.estevesii@gmail.com>
2026-04-15 17:20:18 +0800

    Delete irrelevant files

--- check_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('C:\\Users\\Reymundo\\Desktop\\team-speedrunners-django-system\\db.sqlite3')
-cursor = conn.cursor()
-cursor.execute('SELECT name FROM sqlite_master WHERE type="table"')
-print("Tables:", cursor.fetchall())
-cursor.execute("SELECT sql FROM sqlite_master WHERE type='table' AND name='presenta_chatmessage'")
-print("ChatMessage table:", cursor.fetchone())
-conn.close()
--- check_indexes.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('C:\\Users\\Reymundo\\Desktop\\team-speedrunners-django-system\\db.sqlite3')
-cursor = conn.cursor()
-cursor.execute('SELECT name FROM sqlite_master WHERE type="index" AND name LIKE "%chatmessage%"')
-print(cursor.fetchall())
-conn.close()
